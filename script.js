5cedf0faf50ecfd5c1d1ab842adea24b299e85d2
Victor Todeschini <victodeschini@yahoo.fr>
2019-08-29 22:04:15 +0200

    Modal window

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
+
+let modal = document.getElementById('modal');
+let closeIcon = document.getElementById('closeIcon');
+
 function form01() {
     let answer = document.getElementById('answer').value;
 
     if(answer == 'good') {
         window.location.href = 'page04.html';
     } else {
-        window.location.href = 'page03.html';
+        //window.location.href = 'page03.html';
+
+        modal.style.display = 'block';
+
     }
 }
 
@@ -17,5 +24,16 @@ function unionQuestion() {
         window.location.href = 'page07.html';
     } else {
         window.location.href = 'page06.html';
+    }
+}
+
+
+close.onclick = function() {
+    modal.style.display = 'none';
+}
+
+window.onclick = function(event) {
+    if(event.target == modal) {
+        modal.style.display = 'none';
     }
 }
