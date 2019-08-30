e428a62178b65e71abb7aa98534ab06b912a3760
Victor <victodeschini@yahoo.fr>
2019-08-30 11:41:52 +0200

    WIP countdown

--- script.js
+++ script.js
@@ -24,24 +24,39 @@ function unionQuestion() {
     if(unionAnswer == 'venice beach') {
         window.location.href = 'page07.html';
     } else {
-        window.location.href = 'page06.html';
+        //window.location.href = 'page06.html';
+
+        modal.style.display = 'block';
     }
 }
 
 function veniceQuestion() {
     let veniceAnswer = document.getElementById('veniceAnswer').value;
+    let blockWindow = document.getElementById('blockWindow');
 
     if(veniceAnswer == 'car3') {
         window.location.href = 'page10.html';
     } else {
         modal.style.display = 'block';
+        blockWindow.style.display = 'block';
+        let cpt = 10;
+        let x = setInterval(function() {
+            document.getElementById('countdown').textContent = cpt;
+            
+
+            if(cpt == 0) {
+                clearInterval(x);
+                blockWindow.style.display = 'none';
+            }
+            cpt--;
+        }, 1000);
+
     }
 }
 
 
 closeIcon.onclick = function() {
     modal.style.display = 'none';
-    console.log('HEEEEEEEEY !!!');
 }
 
 window.onclick = function(event) {
